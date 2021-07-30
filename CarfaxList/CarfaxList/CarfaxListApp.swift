9822e7cb195c52bab782bd2fcf59d88c4524c957
Ken Nyame <hmahaddev93@gmail.com>
2021-07-30 03:11:44 -0400

    XCode project created.
    XCode project created.

---
+++ CarfaxList/CarfaxList/CarfaxListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarfaxListApp.swift
+//  CarfaxList
+//
+//  Created by Khateeb Mahad Hussain on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct CarfaxListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
